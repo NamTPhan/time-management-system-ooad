909a092cf4b84121f0d720e5deb6efec81adbf57
Bryn <brynvandam@gmail.com>
2018-10-01 16:58:39 +0200

    #12_taskoverview
    Updated to new version

---
+++ src/main/java/controllers/taskOverviewController.java
@@ -0,0 +1,8 @@
+package controllers;
+
+//import javafx.fxml.FXML;
+
+public class taskOverviewController {
+//    @FXML
+//    private ListView listView;
+}
---
+++ src/main/java/models/SetStage.java
@@ -0,0 +1,37 @@
+/*
+ * Class set stage for switching between screens
+ */
+package models;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * @author Nam
+ */
+public class SetStage {
+
+    //Public constructor
+    public SetStage(Stage stage, String fxmlFile)
+            throws Exception {
+        //Here we have obtained the stage.
+
+        /*Create the root container (the upper structure in an
+            fxml file such as AnchorPane) of the first screen (aka "scene").
+            The root contains all other UI structures as defined in fxml.*/
+        Parent root = FXMLLoader.load(getClass().getResource(fxmlFile));
+
+        //Create a new scene (screen) out of this.
+        Scene scene = new Scene(root);
+
+        //Set this new scene on stage and show it to the user.
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+}
+
+//    Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+//        new SetStage(stage, "/main.fxml");
