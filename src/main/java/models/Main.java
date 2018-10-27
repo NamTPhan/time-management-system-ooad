5a2226be9de63743d8b8db3c994d3805cb3a7ecf
Unknown <namtphann@gmail.com>
2018-10-27 15:18:54 +0200

    ID #5 Added extra settings attribute
    - Added extra settings attribute to be able to select a timer in the settings
    - Main screen selects controller based on timertype from the db
    - Modified some queries

--- src/main/java/models/Main.java
+++ src/main/java/models/Main.java
@@ -11,10 +11,22 @@ public class Main extends Application {
 
     private static final int WINDOW_HEIGHT = 1280, WINDOW_WIDTH = 800;
     private TimerController controller;
+    private final int DEFAULT_SETTINGS_ID = 1;
+    private GenericDAO<Settings> settingsDao = new SettingsDaoImplementation();
+    private Settings settings;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        switch (0) {
+
+        if (settingsDao.getAll().size() == 0){
+            settings = new Settings();
+            settingsDao.save(settings);
+        }
+        
+        settings = settingsDao.getByIndex(DEFAULT_SETTINGS_ID);
+        System.out.println(settings.getSettingsId() + " " + settings.getLengthLongBreak() + " " + settings.getLengthShortBreak());
+
+        switch (settings.getTimerType()) {
             case 1: controller = new HourTimerController(); break;
             case 2: controller = new TenMinuteTimerController(); break;
             default: controller = new PomodoroTimerController(); break;
